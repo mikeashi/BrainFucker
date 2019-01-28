6c8ce565306190c93e37f0b3ae1a7f0519411bd3
MikeAshi <mikeashi2013@gmail.com>
2019-01-28 13:06:05 +0100

    Create FileTest.java
    add File Class Test

---
+++ src/test/java/de/mikeashi/brainfucker/source/FileTest.java
@@ -0,0 +1,35 @@
+package de.mikeashi.brainfucker.source;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTest {
+    private static final String FILENAME = "hello_world.bf";
+    private static final int[] EXPECTED = {62, 43, 43, 43, 43, 43, 43, 43, 43, 91, 45, 60, 43, 43, 43, 43,
+            43, 43, 43, 43, 43, 62, 93, 60, 46, 62, 62, 43, 62, 45, 91, 43, 93, 43, 43, 62, 43, 43, 62, 43,
+            43, 43, 91, 62, 91, 45, 62, 43, 43, 43, 60, 60, 43, 43, 43, 62, 93, 60, 60, 93, 62, 45, 45, 45,
+            45, 45, 46, 62, 45, 62, 43, 43, 43, 46, 46, 43, 43, 43, 46, 62, 45, 46, 60, 60, 43, 91, 62, 91,
+            43, 62, 43, 93, 62, 62, 93, 60, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 46, 62,
+            62, 46, 43, 43, 43, 46, 45, 45, 45, 45, 45, 45, 46, 45, 45, 45, 45, 45, 45, 45, 45, 46, 62, 43,
+            46, 62, 43, 46};
+    private Path path;
+    private File file;
+
+    @BeforeEach
+    void setUp() throws URISyntaxException {
+        path = Paths.get(getClass().getClassLoader().getResource(FILENAME).toURI());
+        file = new File(path);
+    }
+
+    @Test
+    void getCharacterStream() throws IOException {
+        assertArrayEquals(EXPECTED,file.getCharacterStream().toArray());
+    }
+}
