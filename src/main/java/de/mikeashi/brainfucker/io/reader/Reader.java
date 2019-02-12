6c453db8a71725c5422ee12af16641e99cd3cdd0
MikeAshi <mikeashi2013@gmail.com>
2019-02-12 20:09:16 +0100

    Doc
    add JavaDoc

--- src/main/java/de/mikeashi/brainfucker/io/reader/Reader.java
+++ src/main/java/de/mikeashi/brainfucker/io/reader/Reader.java
@@ -1,7 +1,11 @@
 package de.mikeashi.brainfucker.io.reader;
 
 import java.util.Scanner;
-
+/**
+ * the Reader class implements the input interface to provide the read method.
+ *
+ * @author Mike Ashi
+ */
 public class Reader implements input {
     Scanner sc = new Scanner(System.in);
 
--- src/main/java/de/mikeashi/brainfucker/io/reader/input.java
+++ src/main/java/de/mikeashi/brainfucker/io/reader/input.java
@@ -1,5 +1,12 @@
 package de.mikeashi.brainfucker.io.reader;
-
+/**
+ * the input interface which provides the read method.
+ * @author Mike Ashi
+ */
 public interface input {
+    /**
+     * gets a single character from user inputs.
+     * @return
+     */
     byte read();
 }
